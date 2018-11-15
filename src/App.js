b673d30ad5e221d02aaf247b72440acee5629a60
Phat Nguyen <phatnt.uit@gmail.com>
2018-11-15 08:20:32 +0700

    Style app with bootstrap 4

--- src/App.js
+++ src/App.js
@@ -4,84 +4,143 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import null_logo from './null_logo.png'
 
 const App = () => (
-  <div className="container">
-    <div className="sidebar">
-      <div className="line-center">
-        <img src={null_logo} width={"50px"} height={"50px"} className="img img-thumbnail"/>
-        <p>Nguyen Tan Phat</p>
-      </div>
-      <div className="line">
-        <button onClick={() => {alert('create new todo')}} className="btn btn-primary">Create new todo</button>
-      </div>
-      <div className="line">
-        <div className="column">
-          <span>Filters</span>
-          <a href="#">
-            <span>icon</span>
-            <span>Pending</span>
-          </a>
-          <a href="#">
-            <span>icon</span>
-            <span>Not done</span>
-          </a>
-          <a href="#">
-            <span>icon</span>
-            <span>Finished</span>
-          </a>
-          <a href="#">
-            <span>icon</span>
-            <span>Canceled</span>
-          </a>
-        </div>
-      </div>
-      <div className="line">
-        <div className="column">
-          <span>Tags</span>
-          <a href="#">
-            <span>tag_icon</span>
-            <span>Frontend</span>
-          </a>
-          <a href="#">
-            <span>tag_icon</span>
-            <span>Backend</span>
-          </a>
-          <a href="#">
-            <span>tag_icon</span>
-            <span>API</span>
-          </a>
-          <a href="#">
-            <span>tag_icon</span>
-            <span>Issue</span>
-          </a>
-        </div>
-      </div>
-      <div className="line">
-        <div className="column">
-          <span>Priorities</span>
-          <select className="form-control">
-            <option value="all">All</option>
-          </select>
-        </div>
-      </div>
-      <div className="line">
-        <div className="column">
-          <span>Order by</span>
-          <select className="form-control">
-            <option value="az">A to Z</option>
-          </select>
-        </div>
-      </div>
-    </div>
-    <div className="content">
-      <div className="line form-group">
-        <h1>Todo list</h1>
-		<input placeholder="Search todo...." className="right form-control"/>
-      </div>
-	  <div className="line">
-        <h1>Table</h1>
-      </div>
-    </div>
-  </div>
+	<div className="container-fluid">
+		<div className="row header">
+			<div className="col-xl-3">
+				<img src={null_logo} width={"50px"} height={"50px"} className="img img-thumbnail" />
+				<span>Nguyen Tan Phat</span>
+			</div>
+			<div className="col-xl-9">
+				<div className="row">
+				<div className="col-xl-6">
+					<h1>Todo list</h1>
+				</div>
+				<div className="col-xl-6">
+					<input placeholder="Search todo...." className="right mt16 form-control" />
+				</div>
+				</div>
+			</div>
+		</div>
+		<div className="row content">
+			<div className="col-xl-3 sidebar">
+				<button onClick={() => { alert('create new todo') }} className="btn btn-primary">Create new todo</button>
+				<div className="column">
+					<span>Filters</span>
+					<ul>
+						<li>
+							<a href="#">
+								<span>icon</span>
+								<span>Pending</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>icon</span>
+								<span>Not done</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>icon</span>
+								<span>Finished</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>icon</span>
+								<span>Canceled</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="column tag">
+					<span>Tags</span>
+					<ul>
+					<li>
+						<a href="#">
+							<span>tag_icon</span>
+							<span>Frontend</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span>tag_icon</span>
+							<span>Backend</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span>tag_icon</span>
+							<span>API</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span>tag_icon</span>
+							<span>Issue</span>
+						</a>
+					</li>
+					</ul>
+				</div>
+				<div className="column priority">
+					<span>Priorities</span>
+					<select className="form-control">
+						<option value="all">All</option>
+					</select>
+				</div>
+				<div className="column order">
+					<span>Order by</span>
+					<select className="form-control">
+						<option value="az">A to Z</option>
+					</select>
+				</div>
+			</div>
+			<div className="col-xl-9 content">
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th>Sequence</th>
+							<th>Description</th>
+							<th>Tags</th>
+							<th>Priority</th>
+							<th>Assignee</th>
+							<th>Action</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Learn Reactjs</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 )
 
 export default App;
