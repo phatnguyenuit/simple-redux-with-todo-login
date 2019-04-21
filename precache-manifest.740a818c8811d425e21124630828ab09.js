2c940dfa5a2c587169b96f2178ef09fa6f25d378
Nguyen Phat <phatnt.uit@gmail.com>
2019-04-21 22:57:07 +0700

    Updates

---
+++ precache-manifest.740a818c8811d425e21124630828ab09.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/simple-redux-with-todo-login/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "9cc9b111151775b07340",
+    "url": "/simple-redux-with-todo-login/static/css/main.957d3a71.chunk.css"
+  },
+  {
+    "revision": "d2418f4515f6b52bb084",
+    "url": "/simple-redux-with-todo-login/static/js/runtime~main.378e0c3c.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/simple-redux-with-todo-login/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d52bc0a6c66bd948cfd1",
+    "url": "/simple-redux-with-todo-login/static/js/2.cf8129ef.chunk.js"
+  },
+  {
+    "revision": "8868fa3b5de291ec67e7ee41be21bbe3",
+    "url": "/simple-redux-with-todo-login/static/media/null_icon.8868fa3b.png"
+  },
+  {
+    "revision": "9cc9b111151775b07340",
+    "url": "/simple-redux-with-todo-login/static/js/main.0830ba36.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/simple-redux-with-todo-login/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/simple-redux-with-todo-login/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/simple-redux-with-todo-login/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "d52bc0a6c66bd948cfd1",
+    "url": "/simple-redux-with-todo-login/static/css/2.ad58fa1a.chunk.css"
+  },
+  {
+    "revision": "4fa726d896692d8c7f3c67227f33014b",
+    "url": "/simple-redux-with-todo-login/index.html"
+  }
+];
